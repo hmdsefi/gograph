5caa1920a9a3b98f01eab032a91b842041788bc5
Hamed Yousefi <hdyousefi@gmail.com>
2023-02-20 19:21:01 +0330

    implement all directed graph functions

--- graph/directed_graph.go
+++ graph/directed_graph.go
@@ -173,37 +173,155 @@ func (g *DirectedGraph) EdgesOf(v *Vertex) []*Edge {
 	return edges
 }
 
+// RemoveEdges removes input edges from the graph if they exist.
 func (g *DirectedGraph) RemoveEdges(edges ...*Edge) {
-	//TODO implement me
-	panic("implement me")
+	for i := range edges {
+		g.removeEdge(edges[i])
+	}
 }
 
+// removeEdge removes the edge from edges destination map, if size of
+// the internal map is zero, removes the source id from the edges.
+func (g *DirectedGraph) removeEdge(edge *Edge) {
+	if edge == nil {
+		return
+	}
+
+	if g.findVertex(edge.source.id) == nil {
+		return
+	}
+
+	if g.findVertex(edge.dest.id) == nil {
+		return
+	}
+
+	if destMap, ok := g.edges[edge.source.id]; ok {
+		delete(destMap, edge.dest.id)
+		if len(destMap) == 0 {
+			g.removeNeighbor(edge.source.id, edge.dest.id)
+			delete(g.edges, edge.source.id)
+		}
+	}
+}
+
+func (g *DirectedGraph) removeNeighbor(sourceID, neighborID int) {
+	source := g.findVertex(sourceID)
+	for i := range source.neighbors {
+		if source.neighbors[i].id == neighborID {
+			if i == 0 {
+				source.neighbors = source.neighbors[1:]
+			} else if i == len(source.neighbors)-1 {
+				source.neighbors = source.neighbors[:len(source.neighbors)-1]
+			} else {
+				source.neighbors = append(source.neighbors[:i], source.neighbors[i+1:]...)
+			}
+
+			source.neighbors[i].inDegree--
+			source.outDegree--
+			break
+		}
+	}
+}
+
+// GetVertexByID returns the vertex with the input id.
+//
+// If vertex doesn't exist, returns nil.
 func (g *DirectedGraph) GetVertexByID(id int) *Vertex {
-	//TODO implement me
-	panic("implement me")
+	return g.findVertex(id)
 }
 
-func (g *DirectedGraph) GetAllVerticesByID(id ...int) []*Vertex {
-	//TODO implement me
-	panic("implement me")
+// GetAllVerticesByID returns a slice of vertices with the input id list.
+//
+// If vertex doesn't exist, doesn't add nil to the output list.
+func (g *DirectedGraph) GetAllVerticesByID(idList ...int) []*Vertex {
+	var vertices []*Vertex
+	for _, id := range idList {
+		v := g.GetVertexByID(id)
+		if v != nil {
+			vertices = append(vertices, v)
+		}
+	}
+
+	return vertices
 }
 
+// GetAllVertices returns a slice of all existing vertices in the graph.
 func (g *DirectedGraph) GetAllVertices() []*Vertex {
-	//TODO implement me
-	panic("implement me")
+	var vertices []*Vertex
+	for _, vertex := range g.vertices {
+		vertices = append(vertices, vertex)
+	}
+
+	return vertices
 }
 
+// RemoveVertices removes all the specified vertices from this graph including
+// all its touching edges if present.
 func (g *DirectedGraph) RemoveVertices(vertices ...*Vertex) {
-	//TODO implement me
-	panic("implement me")
+	for i := range vertices {
+		g.removeVertex(vertices[i])
+	}
 }
 
+func (g *DirectedGraph) removeVertex(in *Vertex) {
+	if in == nil {
+		return
+	}
+
+	v := g.findVertex(in.id)
+	if v == nil {
+		return
+	}
+
+	for i := range v.neighbors {
+		v.neighbors[i].inDegree--
+	}
+
+	for sourceID := range g.edges {
+		if edge, ok := g.edges[sourceID][v.id]; ok {
+			g.removeEdge(edge)
+			delete(g.edges[sourceID], v.id)
+		}
+	}
+
+	delete(g.edges, v.id)
+	delete(g.vertices, v.id)
+}
+
+// ContainsEdge returns 'true' if and only if this graph contains an edge
+// going from the source vertex to the target vertex.
+//
+// If any of the specified vertices does not exist in the graph, or if is nil,
+// returns 'false'.
 func (g *DirectedGraph) ContainsEdge(from, to *Vertex) bool {
-	//TODO implement me
-	panic("implement me")
+	if from == nil || to == nil {
+		return false
+	}
+
+	if g.findVertex(from.id) == nil {
+		return false
+	}
+
+	if g.findVertex(to.id) == nil {
+		return false
+	}
+
+	if destMap, ok := g.edges[from.id]; ok {
+		if _, ok = destMap[to.id]; ok {
+			return true
+		}
+	}
+
+	return false
 }
 
+// ContainsVertex returns 'true' if this graph contains the specified vertex.
+//
+// If the specified vertex is nil, returns 'false'.
 func (g *DirectedGraph) ContainsVertex(v *Vertex) bool {
-	//TODO implement me
-	panic("implement me")
+	if v == nil {
+		return false
+	}
+
+	return g.findVertex(v.id) != nil
 }
