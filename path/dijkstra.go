73a9fec25e7287f587ec66e9de646a3d4b5a340f
Hamed Yousefi <hdyousefi@gmail.com>
2023-03-18 17:43:31 +0330

    implement dijkstra using min heap

--- path/dijkstra.go
+++ path/dijkstra.go
@@ -1,10 +1,21 @@
 package path
 
-import "github.com/hmdsefi/gograph"
+import (
+	"math"
 
-func Dijkstra[T comparable](g gograph.Graph[T], start T) map[T]float64 {
-	const Inf float64 = 1<<31 - 1
+	"github.com/hmdsefi/gograph"
+	"github.com/hmdsefi/gograph/util"
+)
 
+// DijkstraSimple is a simple implementation of Dijkstra's algorithm. It's using
+// a simple slice to keep track of unvisited vertices, and selected the vertex
+// with the smallest tentative distance using linear search.
+//
+// The time complexity of the simple Dijkstra's algorithm implementation is O(V^2).
+//
+// It returns the shortest distances from the starting vertex to all other vertices
+// in the graph.
+func DijkstraSimple[T comparable](g gograph.Graph[T], start T) map[T]float64 {
 	dist := make(map[T]float64)
 
 	startVertex := g.GetVertexByID(start)
@@ -14,7 +25,7 @@ func Dijkstra[T comparable](g gograph.Graph[T], start T) map[T]float64 {
 
 	vertices := g.GetAllVertices()
 	for _, v := range vertices {
-		dist[v.Label()] = Inf
+		dist[v.Label()] = math.MaxFloat64
 	}
 
 	dist[start] = 0
@@ -36,4 +47,80 @@ func Dijkstra[T comparable](g gograph.Graph[T], start T) map[T]float64 {
 		}
 	}
 	return dist
+}
+
+// dVertex represents dijkstra vertex.
+type dVertex[T comparable] struct {
+	label   T
+	dist    float64
+	visited bool
+	prev    T
+}
+
+func newDVertex[T comparable](label T) *dVertex[T] {
+	return &dVertex[T]{
+		label:   label,
+		dist:    math.MaxFloat64,
+		visited: false,
+	}
+}
+
+// Dijkstra is a standard implementation of Dijkstra's Algorithm that uses a
+// min heap as a priority queue to find the shortest path between start vertex
+// and all other vertices in the specified graph.
+//
+// The time complexity of the standard Dijkstra's algorithm with a min heap is O((E+V)logV).
+//
+// It returns the shortest distances from the starting vertex to all other vertices
+// in the graph.
+func Dijkstra[T comparable](g gograph.Graph[T], start T) map[T]float64 {
+	startVertex := g.GetVertexByID(start)
+	if startVertex == nil {
+		return make(map[T]float64)
+	}
+
+	// Initialize the heap and the visited map
+	pq := util.NewVertexPriorityQueue[T]()
+	visited := make(map[T]bool)
+
+	// Initialize the start vertex
+	verticesMap := make(map[T]*dVertex[T])
+	vertices := g.GetAllVertices()
+	for _, v := range vertices {
+		verticesMap[v.Label()] = newDVertex(v.Label())
+	}
+
+	verticesMap[start].dist = 0
+
+	// Add the start vertex to the heap
+	pq.Push(util.NewVertexWithPriority(g.GetVertexByID(start), verticesMap[start].dist))
+
+	// Main loop
+	for pq.Len() > 0 {
+		// Extract the vertex with the smallest tentative distance from the heap
+		curr := pq.Pop()
+		visited[curr.Vertex().Label()] = true
+
+		// Update the distances of its neighbors
+		neighbors := curr.Vertex().Neighbors()
+		for _, v := range neighbors {
+			if !visited[v.Label()] {
+				neighbor := verticesMap[v.Label()]
+				newDist := curr.Priority() + g.GetEdge(curr.Vertex(), v).Weight()
+				if newDist < neighbor.dist {
+					neighbor.dist = newDist
+					neighbor.prev = curr.Vertex().Label()
+					pq.Push(util.NewVertexWithPriority(v, verticesMap[v.Label()].dist))
+				}
+			}
+		}
+	}
+
+	// Return the distances from the start vertex to each other vertex
+	distances := make(map[T]float64)
+	for _, v := range verticesMap {
+		distances[v.label] = v.dist
+	}
+
+	return distances
 }
--- path/dijkstra_test.go
+++ path/dijkstra_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hmdsefi/gograph"
 )
 
-func TestDijkstra(t *testing.T) {
+func TestDijkstraSimple(t *testing.T) {
 	g := gograph.New[string](gograph.Weighted())
 
 	vA := g.AddVertexByLabel("A")
@@ -20,18 +20,48 @@ func TestDijkstra(t *testing.T) {
 	_, _ = g.AddEdge(vB, vD, gograph.WithEdgeWeight(2))
 	_, _ = g.AddEdge(vC, vD, gograph.WithEdgeWeight(4))
 
-	dist := Dijkstra(g, "A")
+	dist := DijkstraSimple(g, "A")
 
 	if dist[vA.Label()] != 0 {
-		t.Errorf("Expected distance from 0 to 0 to be 0, got %f", dist[vA.Label()])
+		t.Errorf("Expected distance from A to %s to be 0, got %f", vA.Label(), dist[vA.Label()])
 	}
 	if dist[vB.Label()] != 4 {
-		t.Errorf("Expected distance from 0 to 1 to be 4, got %f", dist[vB.Label()])
+		t.Errorf("Expected distance from A to %s  to be 4, got %f", vB.Label(), dist[vB.Label()])
 	}
 	if dist[vC.Label()] != 3 {
-		t.Errorf("Expected distance from 0 to 2 to be 3, got %f", dist[vC.Label()])
+		t.Errorf("Expected distance from A to %s  to be 3, got %f", vC.Label(), dist[vC.Label()])
 	}
 	if dist[vD.Label()] != 6 {
-		t.Errorf("Expected distance from 0 to 3 to be 6, got %f", dist[vD.Label()])
+		t.Errorf("Expected distance from A to %s  to be 6, got %f", vD.Label(), dist[vD.Label()])
+	}
+}
+
+func TestDijkstra(t *testing.T) {
+	g := gograph.New[int](gograph.Weighted())
+
+	v1 := g.AddVertexByLabel(1)
+	v2 := g.AddVertexByLabel(2)
+	v3 := g.AddVertexByLabel(3)
+	v4 := g.AddVertexByLabel(4)
+
+	_, _ = g.AddEdge(v1, v2, gograph.WithEdgeWeight(4))
+	_, _ = g.AddEdge(v1, v3, gograph.WithEdgeWeight(3))
+	_, _ = g.AddEdge(v2, v3, gograph.WithEdgeWeight(1))
+	_, _ = g.AddEdge(v2, v4, gograph.WithEdgeWeight(2))
+	_, _ = g.AddEdge(v3, v4, gograph.WithEdgeWeight(4))
+
+	dist := Dijkstra(g, 1)
+
+	if dist[v1.Label()] != 0 {
+		t.Errorf("Expected distance from 1 to %d to be 0, got %f", v1.Label(), dist[v1.Label()])
+	}
+	if dist[v2.Label()] != 4 {
+		t.Errorf("Expected distance from 1 to %d to be 4, got %f", v2.Label(), dist[v2.Label()])
+	}
+	if dist[v3.Label()] != 3 {
+		t.Errorf("Expected distance from 1 to %d to be 3, got %f", v3.Label(), dist[v3.Label()])
+	}
+	if dist[v4.Label()] != 6 {
+		t.Errorf("Expected distance from 1 to %d to be 6, got %f", v4.Label(), dist[v4.Label()])
 	}
 }
